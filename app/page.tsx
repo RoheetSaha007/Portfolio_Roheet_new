0a713bb8447c5936fb16c05fa700fe02ca2a44ec
Roheet Saha <roheetsaha2955@gmail.com>
2025-08-27 14:30:49 +0530

    Changing my name

--- app/page.tsx
+++ app/page.tsx
@@ -45,7 +45,7 @@ export default function Portfolio() {
     }
 
     // Typewriter effect
-    const text = "Roheet Saha"
+    const text = "Roheet Saha GOD"
     const typingTarget = typingRef.current
     if (typingTarget) {
       let i = 0
